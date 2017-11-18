bf2de0ca0d63a6deee5b284275b753b2a34e74f0
aliasunder <32620895+aliasunder@users.noreply.github.com>
2017-11-18 00:07:15 -0500

    Assignment #2 complete

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,21 +1,32 @@
 // Exercise #1
 // Write your solution below
-
+for (var trianglep = "#"; trianglep.length <= 8; trianglep += "#")
+console.log(trianglep);
 
 /* 
-Write your explanation for Exercise #1 here
-
+To create a loop that logs the triangle to the console, I used a for loop. 
+I initialized the loop by defining the variable "trianglep" as the value "#".
+In the second part of the expression I set the boundary for the variable. 
+Once the length of the "trianglep" value reaches 8, the loop stops.
+In the last part of the expression I specified what happens to the variable after each iteration of the loop.
+For each iteration of this loop, an additional "#"" is added.
+When I call the console.log function, each iteration of the for loop is printed out. Since an additional hash character is added
+on each line, the resulting shape of the printed out hash characters is a triangle.
 */
-
-
-
 
 
 // Exercise #2
 // Write your solution below
-
+function isEven(number) {
+  if (number % 2 == 0)
+  console.log('the number is even!')
+  else 
+    console.log('the number is odd!')
+}
 
 /* 
-Write your explanation for Exercise #2 here
-
+To create a function that checks whether a number is odd or even, I used a conditional statement to evaluate whether a number is
+divisible by 2 using the modulo operator. If the remainder of a number divided by 2 is 0, this means that the number is even. 
+If this condition is met, the first "if" state is executed. 
+If this condition is not met,this means that the number is odd, and therefore the "else" statement is executed. 
 */
